0f7fbcf977a374a5329eaeb9efe73bb887468d1f
Prof. Acenaga <carlos.ferrer@davinci.edu.ar>
2026-02-06 15:30:20 -0300

    added crud for course and relationsmanager comments

--- app/Filament/Resources/Courses/CourseResource.php
+++ app/Filament/Resources/Courses/CourseResource.php
@@ -15,6 +15,7 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use App\Filament\Resources\Courses\RelationManagers\CommentsRelationManager;
 
 class CourseResource extends Resource
 {
@@ -42,7 +43,7 @@ class CourseResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            CommentsRelationManager::class,
         ];
     }
 
---
+++ app/Filament/Resources/Courses/RelationManagers/CommentsRelationManager.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources\Courses\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CommentsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'comments';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('user_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('commentable_type')
+                    ->required(),
+                TextInput::make('commentable_id')
+                    ->required()
+                    ->numeric(),
+                Textarea::make('body')
+                    ->required()
+                    ->columnSpanFull(),
+                Toggle::make('is_approved')
+                    ->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('body')
+            ->columns([
+                TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('commentable_type')
+                    ->searchable(),
+                TextColumn::make('commentable_id')
+                    ->numeric()
+                    ->sortable(),
+                IconColumn::make('is_approved')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AssociateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DissociateAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DissociateBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
